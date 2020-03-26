a3377a4f975e4b9069a5310c8f69ce76b44dd26e
Alin Grecu <56915135+alin-grecu@users.noreply.github.com>
2020-03-26 12:13:17 +0200

    Add / route handler. Refactor functions

--- charts/app.py
+++ charts/app.py
@@ -3,30 +3,30 @@ import pandas as pd
 import datetime as dt
 import re
 
+app = Flask(__name__)
+
 class GraphCorona:
 
     def __init__(self, timeframe, country):
         self.timeframe = timeframe
         self.country = country
         self.data = []
-
-    def run(self):
-        self.generate_dates()
+        self.dates = []
+        self.df = pd.DataFrame(columns=['Country', 'Confirmed'])
 
     def generate_dates(self):
-        dates = []
         for i in range(1, self.timeframe):
             date = dt.datetime.today() - dt.timedelta(days=i)
-            dates.append(date.strftime("%m-%d-%Y"))
-        self.fetch_csv(dates)
+            self.dates.append(date.strftime("%m-%d-%Y"))
+        return self.dates
 
     def fetch_csv(self, dates):
-        df = pd.DataFrame(columns=['Country', 'Confirmed'])
+        self.dates = dates
         repo = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19'
         branch = 'master'
         folder = 'csse_covid_19_data/csse_covid_19_daily_reports/'
         url = repo + '/' + branch + '/' + folder
-        for date in dates:
+        for date in self.dates:
             data_url = url + date + '.csv'
             data = pd.read_csv(data_url, sep=',')
             headers = pd.read_csv(data_url, nrows=0).columns.tolist()
@@ -40,18 +40,22 @@ class GraphCorona:
                 except ValueError:
                     print(ValueError)
             data = data[data['Country'].str.contains(self.country, flags = re.IGNORECASE)]
-            df = df.append(data, ignore_index=True)
-        self.print_csv(df)
+            self.df = self.df.append(data, ignore_index=True)
+        return self.df
 
-    def print_csv(self, data):
-        print(data)
+@app.route('/')
+def bar_chart():
+    bar_dates = dates
+    bar_df = data
+    return render_template('bar_chart.html', title="Confirmed cases in Romania", max=1000, labels=bar_dates, values=bar_df['Confirmed'].tolist())
 
-@app.route('/bar')
-def bar():
-    title="Bar chart by me"
+interval = 10 #days
+country = 'Romania'
 
-corona = GraphCorona(10, 'Romania')
-corona.run()
+corona = GraphCorona(interval, country)
+
+dates = corona.generate_dates()
+data = corona.fetch_csv(dates)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
